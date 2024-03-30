490971835e6d415a28009498795e140c5559b9c9
natnael27 <natnaelmiheret11@gmail.com>
2024-03-30 22:05:24 +0300

    simple java project

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+    Address address1 = new Address("Addis Ababa", 123, "Ethiopia");
+    Employee employee1 = new Employee("Mark Natnael Son", 1, 50000, 
+   address1); 
+    employee1.nameTokenizing();
+    }
+   }
